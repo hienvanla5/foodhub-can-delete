dec73c593927b9c76c6aa06a077b19ce97779508
Hien Van La <lavanhien019250@gmail.com>
2026-05-07 16:04:40 +0700

    Delete js directory

--- js/foods.js
+++
@@ -1,6 +0,0 @@
-export const foods = [
-  { id: 1, name: "Burger", price: 5, image: "assets/images/burger.svg" },
-  { id: 2, name: "Pizza", price: 8, image: "assets/images/pizza.svg" },
-  { id: 3, name: "Sushi", price: 12, image: "assets/images/sushi.svg" },
-  { id: 4, name: "Fries", price: 3, image: "assets/images/fries.svg" },
-];
